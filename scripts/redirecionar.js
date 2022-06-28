43e051bcf8824279c87042ece0b484babf5ddb9c
Aleomar15 <alexandreomar0202@gmail.com>
2022-06-28 16:20:23 -0300

    atualizando

--- scripts/redirecionar.js
+++ scripts/redirecionar.js
@@ -1,10 +1,25 @@
 const Fcar = document.querySelector("#Fcar");
 const Pcar = document.querySelector("#Pcar");
+const Flogin = document.querySelector("#login");
+const Freg = document.querySelector("#register");
+const ir = document.querySelector("#ir");
 Pcar.addEventListener("click",function(c){
     c.preventDefault;
     window.location.href="Pcarona.php";
 });
 Fcar.addEventListener("click",function(c){
     c.preventDefault;
-    window.location.href="index.php";
+    window.location.href="Fcarona.php";
+});
+Flogin.addEventListener("click",function(c){
+    c.preventDefault;
+    window.location.href="Pcarona.php";
+});
+Freg.addEventListener("click",function(c){
+    c.preventDefault;
+    window.location.href="CadstroUser.php";
+});
+ir.addEventListener("click",function(c){
+    c.preventDefault;
+    window.location.href="Pcarona.php";
 });
